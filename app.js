26147e71f205952fb0d5eaa7760eeb3e3f397468
Richmond <toro.richmond.ramil@gmail.com>
2023-11-14 10:34:28 +0800

    commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Bhon Goodluck!");
+});
+var animalsArray = ["Elephant", "Eagle", "Dragonfly"];
+
+// Can you categorize the animalsArray into the following categories?
+
+// Desired Output
+// {
+//     Mammals: [ 'Elephant' ],
+//     Birds: [ 'Eagle' ],
+//     Insects: [ 'Dragonfly' ]
+// }
+
+//
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`);
+});
